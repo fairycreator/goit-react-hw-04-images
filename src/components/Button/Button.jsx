7c1d70fe90d687df3811177efbf20f4489e3d58a
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-08-07 18:08:47 +0200

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../Styles/styles.css';
 
-const Button = ({ onLoadMore, show }) => {
-  return show ? (
+const Button = ({ onLoadMore }) => {
+  return (
     <div>
       <button type="button" className="Button" onClick={onLoadMore}>
         Load more
       </button>
     </div>
-  ) : null;
+  );
 };
 
 export default Button;
